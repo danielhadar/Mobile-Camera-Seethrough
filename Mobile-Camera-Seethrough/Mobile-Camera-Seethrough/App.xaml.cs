452c1f865cac5345dc54b0e186993c3ded5e21da
Daniel Hadar <danielhadar@gmail.com>
2018-05-01 19:04:06 +0300

    Initial commit: Hello world.

---
+++ Mobile-Camera-Seethrough/Mobile-Camera-Seethrough/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace MobileCameraSeethrough
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Mobile_Camera_SeethroughPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Mobile-Camera-Seethrough/Mobile-Camera-Seethrough/Mobile-Camera-SeethroughPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace MobileCameraSeethrough
+{
+    public partial class Mobile_Camera_SeethroughPage : ContentPage
+    {
+        public Mobile_Camera_SeethroughPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
